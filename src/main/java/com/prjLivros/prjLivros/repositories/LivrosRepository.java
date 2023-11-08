288a0a019fc63a5342b8d53c3d5c35947cfdd031
Julia Lumi Suzuki <133894726+JapaLumi@users.noreply.github.com>
2023-11-08 07:10:31 -0300

    Delete src directory

--- src/main/java/com/prjLivros/prjLivros/repositories/LivrosRepository.java
+++
@@ -1,9 +0,0 @@
-package com.prjLivros.prjLivros.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.prjLivros.prjLivros.entities.Livros;
-
-public interface LivrosRepository extends JpaRepository<Livros, Long>{
-
-}
